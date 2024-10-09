cdbeea06b0560d42ef7029040dd8fba9e11ff163
Daniel Lopes Hatae <daniel.hatae@gmail.com>
2024-10-09 18:29:54 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/danielhatae/course/reposiories/OrderItemRepository.java
+++ src/main/java/com/danielhatae/course/reposiories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.danielhatae.course.reposiories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.danielhatae.course.entities.OrderItem;
+import com.danielhatae.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>  {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>  {
 	
 }
